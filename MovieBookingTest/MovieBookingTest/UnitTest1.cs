32a411b0329059e18ee07b40703d622679c108ec
Yimele <bapfou@yahoo.co.uk>
2017-09-04 12:17:31 +0100

    Final Movie Booking Project

---
+++ MovieBookingTest/MovieBookingTest/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MovieBooking.Controllers;
+using MovieBooking.Models;
+
+
+namespace MyMovieUnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            CustomersController cc = new CustomersController();
+            Customer c = cc.Details(1);
+            Assert.AreEqual("Tom", c.FirstName);
+        }
+    }
+}
+
